e815df5c55af50845286365dc7a7bc568f27ffd1
Nagaratna Karetimmannanavar <nagaratnakaretimmannanavar@Nagaratnas-MacBook-Pro.local>
2022-10-25 20:04:41 -0700

    update

---
+++ env-prod/prod-backend.tfvars
@@ -0,0 +1,3 @@
+bucket = "devws-tf-state-bucket"
+key    = "databases/dev/terrafom.tfstate"
+region = "us-east-1"
---
+++ main.tf
@@ -0,0 +1,9 @@
+module "mongodb" {
+  source              = "./vendor/modules/mongodb"
+  ENV                 = var.ENV
+}
+
+module "redis" {
+  source              = "./vendor/modules/redis"
+  ENV                 = var.ENV
+}
